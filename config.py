0c8e7a24b26592768cc030b7513b4f0547d21a77
DERgogo <gsilic@gmx.de>
2025-11-18 20:40:56 +0100

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "supersecret123")
 WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
 
-# ❗ Hier die **echte Railway-URL** deines Projekts eintragen:
-WEBHOOK_URL = f"https://<DEIN_PROJEKT>.up.railway.app{WEBHOOK_PATH}"
+# Trage hier DEINE Railway-URL ein
+WEBHOOK_URL = f"https://<dein-projektname>.up.railway.app{WEBHOOK_PATH}"
