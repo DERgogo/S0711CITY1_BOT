4d5b2774dea38f147b4da8dc360999de3127c09f
DERgogo <gsilic@gmx.de>
2025-11-18 18:45:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import uvicorn
 from app.web import fastapi_app, telegram_app
+import uvicorn
 from app.config import WEBHOOK_URL
-import asyncio
-async def on_startup():
+
+async def start_webhook():
     await telegram_app.bot.set_webhook(WEBHOOK_URL)
-if __name__=='__main__':
-    asyncio.run(on_startup())
-    uvicorn.run(fastapi_app,host='0.0.0.0',port=8080)
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(start_webhook())
+    uvicorn.run("app.web:fastapi_app", host="0.0.0.0", port=8000)
